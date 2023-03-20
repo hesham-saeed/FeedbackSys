a470fe698a2ea6558567053038649f78918efccc
Hesham <heshamsaid@hotmail.com>
2023-03-20 23:54:32 +0100

    initial commit

---
+++ feedback/about.php
@@ -0,0 +1,6 @@
+<?php include 'inc/header.php'; ?>
+
+    <h2>About</h2>
+
+    <p class="text-center">The Erasmus Mundus European Masters Program in Language and Communication Technologies (LCT) is designed to meet the demands of industry and research in a rapidly growing area. It offers education and training opportunities for the next generations of leaders in research and innovation. It provides students with profound knowledge and insight into the various disciplines that contribute to the methods of language and communication technologies and it strengthens their ability to work according to scientific methods. Moreover, the students acquire practice-oriented knowledge by choosing appropriate combinations of modules in Language Technology, Computational and Theoretical Linguistics, and Computer Science.</p>
+<?php include 'inc/footer.php'; ?>
